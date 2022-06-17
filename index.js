1cb25697f3e249925acc7fb8fdd9f60098ac8008
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-17 07:18:44 +0300

    Done

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //function isPalindrome should return true if it reads same forward and backward
+  //it should return false if it does not read same forward and backward
+  //should only be lowercase
+  let reverseWord=word.split("").reverse().join("")
+  if(word==reverseWord){
+    return true;
+  }
+  else{
+    return false;
+  }
+
 }
+console.log(isPalindrome("mom"))
 
 /* 
   Add your pseudocode here
+  declare a fuction Palindrome
+
 */
 
 /*
